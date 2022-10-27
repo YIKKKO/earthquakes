3b5178a2bfc7003a5c4886c36b33224ba78db94a
Yi Gu <yi.gu.17@ucl.ac.uk>
2022-10-27 10:44:02 +0100

    1

--- earthquakes.py
+++ earthquakes.py
@@ -3,6 +3,7 @@
 # However, we will use a more powerful and simpler library called requests.
 # This is external library that you may need to install first.
 import requests
+import json
 
 
 def get_data():
@@ -31,27 +32,46 @@ def get_data():
 
     # We need to interpret the text to get values that we can work with.
     # What format is the text in? How can we load the values?
-    return ...
+    return json.loads(text)
 
 def count_earthquakes(data):
     """Get the total number of earthquakes in the response."""
-    return ...
+    return data["metadata"]["count"]
 
 
 def get_magnitude(earthquake):
     """Retrive the magnitude of an earthquake item."""
-    return ...
+    return earthquake["properties"]["mag"]
 
 
 def get_location(earthquake):
     """Retrieve the latitude and longitude of an earthquake item."""
+    coordinates = earthquake["geometry"]["coordinates"]
     # There are three coordinates, but we don't care about the third (altitude)
-    return ...
+    return (coordinates[0], coordinates[1])
 
 
 def get_maximum(data):
     """Get the magnitude and location of the strongest earthquake in the data."""
-    ...
+    current_max_magnitude = get_magnitude(data["features"][0])
+    current_max_location = get_location(data["features"][0])
+    for item in data["features"]:
+        magnitude = get_magnitude(item)
+        # Note: what happens if there are two earthquakes with the same magnitude?
+        if magnitude > current_max_magnitude:
+            current_max_magnitude = magnitude
+            current_max_location = get_location(item)
+    return current_max_magnitude, current_max_location
+    # There are other ways of doing this too:
+    # biggest_earthquake = sorted(data["features"], key=get_magnitude)[0]
+    # return get_magnitude(biggest_earthquake), get_location(biggest_earthquake)
+    # Or...
+    # biggest_earthquake = max(
+    #     ({"mag": get_magnitude(item), "location": get_location(item)}
+    #     for item in data["features"]),
+    #     key=lambda x: x["mag"]
+    # )
+    # return biggest_earthquake["mag"], biggest_earthquake["location"]
 
 
 # With all the above functions defined, we can now call them and get the result
